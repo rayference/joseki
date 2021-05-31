854cfe5c21f7877668173f16f33fb2e7850f115e
Yvan Nollet <yvan.nollet@rayference.eu>
2021-05-31 13:57:34 +0200

    Added intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -7,9 +7,15 @@ author = "Yvan Nollet"
 copyright = f"{datetime.now().year}, {author}"
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
     "sphinx.ext.napoleon",
     "sphinx_click",
     "sphinx_rtd_theme",
 ]
 autodoc_typehints = "description"
 html_theme = "sphinx_rtd_theme"
+intersphinx_mapping = {
+    "pandas": ("https://pandas.pydata.org/docs/", None),
+    "pint": ("https://pint.readthedocs.io/en/stable/", None),
+    "xarray": ("http://xarray.pydata.org/en/stable/", None),
+}
