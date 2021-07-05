68d7198c1a9fefb807cde8da04990f399f87a991
Yvan Nollet <yvan.nollet@rayference.eu>
2021-07-05 18:59:31 +0200

    Added tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,10 @@
 """Test cases for the __main__ module."""
+import pathlib
 from typing import Any
 
+import numpy as np
 import pytest
+import xarray as xr
 from click.testing import CliRunner
 
 from joseki import __main__
@@ -18,6 +21,40 @@ def test_main_succeeds(runner: CliRunner, tmpdir: Any) -> None:
     result = runner.invoke(
         __main__.main,
         ["--identifier=afgl_1986-tropical", f"--file-name={tmpdir / 'ds.nc'}"],
+    )
+    assert result.exit_code == 0
+
+
+def test_main_open_data_set(runner: CliRunner, tmpdir: Any) -> None:
+    """Returns a xarray.Dataset."""
+    path = pathlib.Path(tmpdir / "ds.nc")
+    runner.invoke(
+        __main__.main,
+        ["--identifier=afgl_1986-tropical", f"--file-name={path}"],
+    )
+    assert isinstance(xr.open_dataset(path), xr.Dataset)
+
+
+@pytest.fixture
+def altitudes_path(tmpdir: Any) -> pathlib.Path:
+    """Fixture for altitudes file path."""
+    z_values = np.linspace(0, 120, 121)
+    path = pathlib.Path(tmpdir, "z.txt")
+    np.savetxt(path, z_values)
+    return path
+
+
+def test_main_altitude_path(
+    runner: CliRunner, tmpdir: Any, altitudes_path: pathlib.Path
+) -> None:
+    """."""
+    result = runner.invoke(
+        __main__.main,
+        [
+            "--identifier=afgl_1986-tropical",
+            f"--file-name={tmpdir / 'ds.nc'}",
+            f"--altitudes={altitudes_path}",
+        ],
     )
     assert result.exit_code == 0
 
