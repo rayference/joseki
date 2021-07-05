e992b1bd881b1fe4fbb7f425ed607402a99e553e
Yvan Nollet <yvan.nollet@rayference.eu>
2021-07-05 14:11:45 +0200

    Added a test

--- tests/test_main.py
+++ tests/test_main.py
@@ -20,3 +20,16 @@ def test_main_succeeds(runner: CliRunner, tmpdir: Any) -> None:
         ["--identifier=afgl_1986-tropical", f"--file-name={tmpdir / 'ds.nc'}"],
     )
     assert result.exit_code == 0
+
+
+def test_main_set_main_coord_to_layer_altitude(runner: CliRunner, tmpdir: Any) -> None:
+    """Exits with a status code of zero with option '-s'."""
+    result = runner.invoke(
+        __main__.main,
+        [
+            "--identifier=afgl_1986-tropical",
+            f"--file-name={tmpdir / 'ds.nc'}",
+            "--set-main-coord-to-layer-altitude",
+        ],
+    )
+    assert result.exit_code == 0
