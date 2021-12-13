63e930f6eca4fb5e634896b3e37ae0f2bc70800b
Yvan Nollet <yvan.nollet@rayference.eu>
2021-12-13 11:03:37 +0100

    Removed python 3.10 support + poetry update

--- noxfile.py
+++ noxfile.py
@@ -10,7 +10,7 @@ from nox.sessions import Session
 
 
 package = "joseki"
-python_versions = ["3.10", "3.9", "3.8", "3.7"]
+python_versions = ["3.9", "3.8", "3.7"]
 nox.options.sessions = (
     "pre-commit",
     "safety",
