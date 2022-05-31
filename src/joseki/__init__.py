287c098dab12e2e55047787be736d42aa3cacb14
Yvan Nollet <yvan.nollet@rayference.eu>
2022-05-31 16:49:28 +0200

    Expose unit registry

--- src/joseki/__init__.py
+++ src/joseki/__init__.py
@@ -2,10 +2,12 @@
 from . import accessor
 from .core import Identifier  # pyflakes.ignore
 from .core import make  # pyflakes.ignore
+from .units import ureg as unit_registry
 
 
 __all__ = [
     "accessor",
     "Identifier",
     "make",
+    "unit_registry",
 ]
