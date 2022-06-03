831403eecf23258550f32f36feb5264fb5aabe0a
Yvan Nollet <62285993+nollety@users.noreply.github.com>
2022-06-03 22:23:12 +0200

    Add scaling factors accessor (#225)
    * Implemented scaling_factors accessor
    
    * Add tests and fix accessor
    
    * First line should end with a period
    
    * Add missing type hints
    
    * Fix line too long

--- src/joseki/accessor.py
+++ src/joseki/accessor.py
@@ -27,6 +27,46 @@ def molecular_mass(m: str) -> pint.Quantity:  # type: ignore[type-arg]
     return ureg.Quantity(float(molmass.Formula(m).mass), "dalton")
 
 
+def _scaling_factor(
+    initial_amount: pint.Quantity,  # type: ignore[type-arg]
+    target_amount: pint.Quantity,  # type: ignore[type-arg]
+) -> float:
+    """
+    Compute scaling factor given initial and target amounts.
+
+    Parameters
+    ----------
+    initial_amount: :class:`~pint.Quantity`
+        Initial amount.
+
+    target_amount: :class:`~pint.Quantity`
+        Target amount.
+
+    Raises
+    ------
+    ValueError
+        when the initial amount has a zero magnitude and the target amount
+        has a non-zero magnitude.
+
+    Returns
+    -------
+    float
+        Scaling factor
+    """
+    if initial_amount.m == 0.0:
+        if target_amount.m == 0.0:
+            return 0.0
+        else:
+            raise ValueError(
+                f"Cannot compute scaling factor when initial amount has "
+                f"magnitude of zero and target amount has a non-zero magnitude "
+                f"(got {target_amount})."
+            )
+    else:
+        factor = (target_amount / initial_amount).m_as(ureg.dimensionless)
+        return float(factor)
+
+
 @xr.register_dataset_accessor("joseki")  # type: ignore[no-untyped-call]
 class JosekiAccessor:  # pragma: no cover
     """Joseki accessor."""
@@ -149,7 +189,7 @@ class JosekiAccessor:  # pragma: no cover
     def mass_density_at_sea_level(
         self,
     ) -> t.Dict[str, pint.Quantity]:  # type: ignore[type-arg]
-        """Compute mass mass density at sea level.
+        """Compute mass density at sea level.
 
         Returns
         -------
@@ -161,6 +201,80 @@ class JosekiAccessor:  # pragma: no cover
             m: (molecular_mass(m) * _number_density_at_sea_level[m]).to("kg/m^3")
             for m in self._obj.m.values
         }
+
+    @property
+    def volume_mixing_fraction_at_sea_level(
+        self,
+    ) -> t.Dict[str, pint.Quantity]:  # type: ignore[type-arg]
+        """Compute volume mixing fraction at sea level.
+
+        Returns
+        -------
+        dict:
+            A mapping of molecule and volume mixing fraction at sea level.
+        """
+        ds = self._obj
+        return {m: to_quantity(ds.x.sel(m=m).isel(z=0)) for m in ds.m.values}
+
+    def scaling_factors(
+        self, target: t.MutableMapping[str, pint.Quantity]  # type: ignore[type-arg]
+    ) -> t.MutableMapping[str, float]:
+        """
+        Compute scaling factor(s) to reach specific target amount(s).
+
+        Seealso
+        -------
+        :meth:`rescale`
+
+        Parameters
+        ----------
+        target
+            Mapping of molecule and target amount.
+
+        Raises
+        ------
+        ValueError
+            If a target amount has dimensions that are not supported.
+
+        Returns
+        -------
+        dict
+            Mapping of molecule and scaling factors.
+
+        Notes
+        -----
+        For each molecule in the ``target`` mapping, the target amount is
+        interpreted, depending on its dimensions (indicated in square brackets),
+        as:
+
+        * a column number density [``length^-2``],
+        * a column mass density [``mass * length^-2``],
+        * a number densitx at sea level [``length^-3``],
+        * a mass density at sea level [``mass * length^-3``],
+        * a volume mixing fraction at sea level [``dimensionless``]
+
+        The scaling factor is then evaluated as the ratio of the target amount
+        with the original amount, for each molecule.
+        """
+        compute_initial_amount = {
+            "[length]^-2": self.column_number_density,
+            "[mass] * [length]^-2": self.column_mass_density,
+            "[length]^-3": self.number_density_at_sea_level,
+            "[mass] * [length]^-3": self.mass_density_at_sea_level,
+            "": self.volume_mixing_fraction_at_sea_level,
+        }
+        factors = {}
+        for m, target_amount in target.items():
+            initial_amount = None
+            for dim in compute_initial_amount.keys():
+                if target_amount.check(dim):
+                    initial_amount = compute_initial_amount[dim][m]
+            if initial_amount is None:
+                raise ValueError
+            factors[m] = _scaling_factor(
+                initial_amount=initial_amount, target_amount=target_amount
+            )
+        return factors
 
     def rescale(self, factors: t.MutableMapping[str, float]) -> None:
         """Rescale molecules concentration in atmospheric profile.
